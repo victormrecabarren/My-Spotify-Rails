ca40ccc2c9fc07dbb704b02a8d15db77a61f2b13
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-13 09:59:16 -0400

    added functions to create album track and artist arrays separate from set_search method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -18,37 +18,22 @@ class SearchController < ApplicationController
 
     spotify_results = RSpotify::Base.search(params[:id], 'artist, album, track')
 
-    @artists = spotify_results.select{|result|result.type==="artist"}.map{|artist| artist = {
-      artist_id: artist.id,
-      name: artist.name,
-      img: artist.images,
-      tracks: [],
-      type: artist.type
-      }
-    }
-    @albums = spotify_results.select{|result|result.type==="album"}.map{|album| album = {
-      album_id: album.id,
-      name: album.name,
-      artist: album.artists[0].name,
-      artist_id: album.artists[0].id,
-      img: album.images,
-      tracks: [],
-      type: album.type
-      }
-    }
-    @tracks = spotify_results.select{|result|result.type==="track"}.map{|track| track = {
-      track_id: track.id,
-      track_number: track.track_number,
-      name: track.name,
-      explicit: track.explicit,
-      artist: track.artists[0].name,
-      artist_id: track.artists[0].id,
-      album: track.album.name,
-      album_id: track.album.id,
-      img: track.album.images,
-      type: track.type
-      }
-    }
+    @topResult = spotify_results[0]
+
+    if @topResult.type==="artist" then
+      @topResult = set_artist [@topResult]
+    elsif @topResult.type==="album" then
+      @topResult = set_album [@topResult]
+    elsif @topResult.type==="track" then
+      @topResult = set_track [@topResult]
+    end
+
+    @artists = set_artist spotify_results.select{|result|result.type==="artist"}
+
+
+    @albums =  set_album spotify_results.select{|result|result.type==="album"}
+
+    @tracks = set_track spotify_results.select{|result|result.type==="track"}
 
 
     #
@@ -73,5 +58,46 @@ class SearchController < ApplicationController
     #      }
     #    }
   end
+
+  def set_album album_array
+    album_array.map{|album| album = {
+      album_id: album.id,
+      name: album.name,
+      artist: album.artists[0].name,
+      artist_id: album.artists[0].id,
+      img: album.images,
+      tracks: [],
+      type: album.type
+      }
+    }
+  end
+
+  def set_artist artist_array
+    artist_array.map{|artist| artist = {
+      artist_id: artist.id,
+      name: artist.name,
+      img: artist.images,
+      tracks: [],
+      type: artist.type
+      }
+    }
+  end
+
+  def set_track track_array
+    track_array.map{|track| track = {
+      track_id: track.id,
+      track_number: track.track_number,
+      name: track.name,
+      explicit: track.explicit,
+      artist: track.artists[0].name,
+      artist_id: track.artists[0].id,
+      album: track.album.name,
+      album_id: track.album.id,
+      img: track.album.images,
+      type: track.type
+      }
+    }
+  end
+
 
 end
