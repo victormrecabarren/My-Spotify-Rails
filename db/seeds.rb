33e4e8c23e046be74546276964cf2fc543018375
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-14 18:18:05 -0400

    deleted spotify model for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,41 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Song.create([
-    {
-      title: "Distorted Records",
-      album: "Testing",
-      artist: "A$AP Rocky",
-      img: "Testing.jpg"
-    },
-    {
-      title: "Provider",
-      album: "Provider",
-      artist: "Frank Ocean",
-      img: "provider.jpeg"
-    },
-    {
-      title: "20 Something",
-      album: "ctrl",
-      artist: "SZA",
-      img: "ctrl.jpg"
-    },
-    {
-      title: "Wasted Times",
-      album: "My Dear Meloncholy,",
-      artist: "The Weeknd",
-      img: "meloncholy.jpeg"
-    },
-    {
-      title: "If You Know You Know",
-      album: "DAYTONA",
-      artist: "Pusha T",
-      img: "daytona.jpg"
-    },
-    {
-      title: "Foreword",
-      album: "Flower Boy",
-      artist: "Tyler, The Creator",
-      img: "flowerboy.jpg"
-    },
-  ])
+# Song.create([
+#     {
+#       title: "Distorted Records",
+#       album: "Testing",
+#       artist: "A$AP Rocky",
+#       img: "Testing.jpg"
+#     },
+#     {
+#       title: "Provider",
+#       album: "Provider",
+#       artist: "Frank Ocean",
+#       img: "provider.jpeg"
+#     },
+#     {
+#       title: "20 Something",
+#       album: "ctrl",
+#       artist: "SZA",
+#       img: "ctrl.jpg"
+#     },
+#     {
+#       title: "Wasted Times",
+#       album: "My Dear Meloncholy,",
+#       artist: "The Weeknd",
+#       img: "meloncholy.jpeg"
+#     },
+#     {
+#       title: "If You Know You Know",
+#       album: "DAYTONA",
+#       artist: "Pusha T",
+#       img: "daytona.jpg"
+#     },
+#     {
+#       title: "Foreword",
+#       album: "Flower Boy",
+#       artist: "Tyler, The Creator",
+#       img: "flowerboy.jpg"
+#     },
+#   ])
+
+Playlist.create({
+  playlist_name: 'new playlist'
+  })
