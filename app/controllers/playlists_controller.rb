70c9982d4d37c3cb10e00ae238371ce2ae6951a8
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-14 23:21:00 -0400

    changed key selection to [:id] syntax

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -24,16 +24,16 @@ class PlaylistsController < ApplicationController
     containing_album = params[:playlist][:seed_album]
 
     puts "var seedtrack = #{seed_track}"
-    puts "seedtrack.id is #{seed_track.id}"
+    puts "seedtrack.id is #{seed_track[:id]}"
 
     Track.new({
-      spotify_id: seed_track.id,
-      playlist_id: @playlist.id,
-      name: seed_track.name,
-      artist: containing_album.artist,
-      image: containing_album.images[0].url,
-      album_name: containing_album.name,
-      album_id: containing_album.id,
+      spotify_id: seed_track[:id],
+      playlist_id: @playlist[:id],
+      name: seed_track[:name],
+      artist: containing_album[:artist],
+      image: containing_album.[:images][0][:url],
+      album_name: containing_album[:name],
+      album_id: containing_album[:id],
       })
 
     if @playlist.save
